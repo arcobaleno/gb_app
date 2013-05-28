3e72bfcaddbff3d798e9f07ab13e58dedec9abc1
Van Michael <van.michael.nguyen@gmail.com>
2013-05-28 03:22:28 -0400

    search bar no func

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,18 @@
 $(function(){
   $('#myCarousel').carousel();
 });
+
+$(function(){ //Document ready shorthand
+	var $search = $('#search');//Cache the element for faster DOM searching since we are using it more than once
+	original_val = $search.val(); //Get the original value to test against. We use .val() to grab value="Search"
+	$search.focus(function(){ //When the user tabs/clicks the search box.
+		if($(this).val()===original_val){ //If the value is still the default, in this case, "Search"
+			$(this).val('');//If it is, set it to blank
+		}
+	})
+	.blur(function(){//When the user tabs/clicks out of the input
+		if($(this).val()===''){//If the value is blank (such as the user clicking in it and clicking out)...
+			$(this).val(original_val); //... set back to the original value
+		}
+	});
+});
