207adc14004220c2cbf59c26214e331c69badb9d
Van Michael <van.michael.nguyen@gmail.com>
2013-05-28 16:47:44 -0400

    eleventh commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 GbApp::Application.routes.draw do
-   #root to: 'static_pages#home'
+   root to: 'static_pages#home'
 
    match "home" => "static_pages#home"
    match "about" => "static_pages#about"
