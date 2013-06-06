8d939c7866f3246f5c1bfdfe6ee9a9cf3bb520f1
Van Michael <van.michael.nguyen@gmail.com>
2013-06-05 21:37:33 -0400

    Fix home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,20 @@ class StaticPagesController < ApplicationController
   def home
   		
   end
+
+  def about
+
+  end
+
+  def contact
+  	
+  end
+
+  def events
+
+  end
+
+  def opportunities
+  	
+  end
 end
